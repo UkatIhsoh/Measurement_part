d3ae7b236333fde30a842f8bea9042fa0f62d481
Ukatihsoh <absolute910@gmail.com>
2019-11-27 17:52:39 +0900

    シンプルなカウンターにならってカウンター部分を変更。定数データを使用した際にカウンターの正常な動作を確認。

--- timekeeper.vhd
+++ timekeeper.vhd
@@ -58,12 +58,12 @@ end timekeeper;
 architecture count_time of timekeeper is
 
 	signal counter : std_logic_vector(63 downto 0):= (others => '0'); 		--カウンター
-	signal count_num : std_logic_vector(63 downto 0):=X"00000000000000FF";		--カウント上限
+	signal count_num : std_logic_vector(63 downto 0):=X"00000000000186A0";		--カウント上限
 	signal data_en : std_logic:= '0'; 									--データがかわっているかのチェック
-	signal en_count : std_logic_vector(7 downto 0):=(others => '0'); 		--出力がhighになっている時間のカウント
+--	signal en_count : std_logic_vector(7 downto 0):=(others => '0'); 		--出力がhighになっている時間のカウント
 	signal out_sig : std_logic:='0'; 									--出力信号
 
-	constant en_time : std_logic_vector(7 downto 0):= X"FF"; --出力high時間
+	constant en_time : std_logic_vector(63 downto 0):= X"00000000000186A0"; --出力high時間
 	
 begin
 
@@ -75,41 +75,51 @@ begin
 		if rst = '1' then
 			--count_num <= (others => '0');
 			data_en <= '0';
-			en_count <= (others => '0');
+			--en_count <= (others => '0');
 			out_sig <= '0';
 			counter <= (others => '0');
 		elsif clk' event and clk = '1' then
-			if out_sig = '1' then
-				if en_count = en_time then --出力信号highの間の時間をカウントし、設定した時間になったら出力をlowにする
-					en_count <= (others =>'0');
-					out_sig <= '0';
-				else
-					en_count <= en_count +1; 
-				end if;
-			else
-				if data_en = '1' then
-					if counter = count_num then
+			if cnt_start = '1' then
+				if out_sig = '1' then
+--					if en_count = en_time then --出力信号highの間の時間をカウントし、設定した時間になったら出力をlowにする
+					if counter = en_time then
+--						en_count <= (others =>'0');
 						counter <= (others => '0');
-						out_sig <= '1';
-					else
-						counter <= counter +1;
 						out_sig <= '0';
+					else
+--						en_count <= en_count +1; 
+						counter <= counter +1;
 					end if;
 				else
-					out_sig <= '0';
+					if data_en = '1' then
+						if counter = count_num then
+							counter <= (others => '0');
+							out_sig <= '1';
+						else
+							counter <= counter +1;
+							out_sig <= '0';
+						end if;
+					else
+						counter <= (others => '0');
+--						count_num <= data;
+						out_sig <= '0';
+						data_en <= '1';
+					end if;
 				end if;
+			else
+				data_en <= '0';
 			end if;
 		end if;
 		
-		if cnt_start = '1' then --cnt_start(カウントスタート)がイネーブル中にカウント
-			if data_en = '0' then
-				counter <= (others => '0');
-				--count_num <= data;
-				data_en <= '1';
-			end if;
-		else 
-			data_en <= '0';
-		end if;
+--		if cnt_start = '1' then --cnt_start(カウントスタート)がイネーブル中にカウント
+--			if data_en = '0' then
+--				counter <= (others => '0');
+--				--count_num <= data;
+--				data_en <= '1';
+--			end if;
+--		else 
+--			data_en <= '0';
+--		end if;
 		
 	end process;
 
